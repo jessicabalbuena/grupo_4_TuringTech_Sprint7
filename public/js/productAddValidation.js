a9e7fa37a4780898a5680995db017c3c62cdca3c
jessicabalbuena <jessbalbuenam@gmail.com>
2022-03-13 03:07:12 -0300

    Se agregaron algunas modificaciones de las validaciones

--- public/js/productAddValidation.js
+++ public/js/productAddValidation.js
@@ -8,5 +8,89 @@ const expresiones = {
   productDescriptionLong: /^[a-zA-ZÀ-ÿ\s]{10,40}$/, //  Letras y espacios, pueden llevar acentos, igual que productName pero mayor cantidad de carecteres.
   productPrice: /^\d{2,14}$/, // 2 a 14 numeros
   productStock: /^\d{1,14}$/, // 1 a 14 numeros
-  productFees: /^\d{0,14}$/, // 0 a 12 numeros
+  productFees: /^\d{1,12}$/, // 0 a 12 numeros
 };
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "productName":
+      if (expresiones.usuario.test(e.target.value)) {
+        document
+          .querySelector("#grupo__nombre .formulario__input-error")
+          .classList.remove("formulario__input-error-activo");
+      } else {
+        document
+          .querySelector("#grupo__nombre .formulario__input-error")
+          .classList.add("formulario__input-error-activo");
+      }
+      break;
+
+    case "productDescription":
+      if (expresiones.productDescription.test(e.target.value)) {
+        document
+          .querySelector("#grupo__descripcion .formulario__input-error")
+          .classList.remove("formulario__input-error-activo");
+      } else {
+        document
+          .querySelector("#grupo__descripcion .formulario__input-error")
+          .classList.add("formulario__input-error-activo");
+      }
+      break;
+
+    case "productDescriptionLong":
+      if (expresiones.productDescriptionLong.test(e.target.value)) {
+        document
+          .querySelector("#grupo__descripcionLong .formulario__input-error")
+          .classList.remove("formulario__input-error-activo");
+      } else {
+        document
+          .querySelector("#grupo__descripcionLong .formulario__input-error")
+          .classList.add("formulario__input-error-activo");
+      }
+      break;
+
+    case "productPrice":
+      if (expresiones.productPrice.test(e.target.value)) {
+        document
+          .querySelector("#grupo__productPrice .formulario__input-error")
+          .classList.remove("formulario__input-error-activo");
+      } else {
+        document
+          .querySelector("#grupo__productPrice .formulario__input-error")
+          .classList.add("formulario__input-error-activo");
+      }
+      break;
+
+    case "productStock":
+      if (expresiones.productStock.test(e.target.value)) {
+        document
+          .querySelector("#grupo__productStock .formulario__input-error")
+          .classList.remove("formulario__input-error-activo");
+      } else {
+        document
+          .querySelector("#grupo__productStock  .formulario__input-error")
+          .classList.add("formulario__input-error-activo");
+      }
+      break;
+
+    case "productFees":
+      if (expresiones.productFees.test(e.target.value)) {
+        document
+          .querySelector("#grupo__productFees .formulario__input-error")
+          .classList.remove("formulario__input-error-activo");
+      } else {
+        document
+          .querySelector("#grupo__productFees .formulario__input-error")
+          .classList.add("formulario__input-error-activo");
+      }
+      break;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
