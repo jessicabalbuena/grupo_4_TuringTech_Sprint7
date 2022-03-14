c78cd942501f4c81bf6bfeaa2c2677bb5cbca7e4
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-03-14 18:23:31 -0300

    se agrega submit

--- public/js/userRegisterValidation.js
+++ public/js/userRegisterValidation.js
@@ -12,6 +12,9 @@ const expresiones = {
 	zipcode: /^\d{1,5}$/
 }
 
+let errores = [];
+
+
 const validarFormulario = (e) => {
 switch (e.target.name){
 	case "registroFullname":
@@ -21,10 +24,16 @@ switch (e.target.name){
 			document.getElementById("p_error1").classList.remove("error_activo")
 			document.getElementById("registroNombre").classList.add("borde_correcto_activo")
 			document.getElementById("registroNombre").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
+			
 		} else {
 			document.getElementById("p_error1").classList.add("error_activo")
 			document.getElementById("registroNombre").classList.add("borde_incorrecto_activo")
 			document.getElementById("registroNombre").classList.remove("borde_correcto_activo")
+		 errores.push("1")
+		console.log(errores);
 			
 		}
 	break;
@@ -35,11 +44,14 @@ switch (e.target.name){
 			document.getElementById("p_error2").classList.remove("error_activo")
 			document.getElementById("registroUsuario").classList.add("borde_correcto_activo")
 			document.getElementById("registroUsuario").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error2").classList.add("error_activo")
 			document.getElementById("registroUsuario").classList.remove("borde_correcto_activo")
 			document.getElementById("registroUsuario").classList.add("borde_incorrecto_activo")
-			
+			errores.push("2")
 		}
 	break;
 	case "registroEmail":
@@ -49,10 +61,14 @@ switch (e.target.name){
 			document.getElementById("p_error3").classList.remove("error_activo")
 			document.getElementById("registroEmail").classList.add("borde_correcto_activo")
 			document.getElementById("registroEmail").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error3").classList.add("error_activo")
 			document.getElementById("registroEmail").classList.remove("borde_correcto_activo")
 			document.getElementById("registroEmail").classList.add("borde_incorrecto_activo")
+			errores.push("3")
 		}
 	break;
 	case "registroDni":
@@ -62,10 +78,14 @@ switch (e.target.name){
 			document.getElementById("p_error4").classList.remove("error_activo")
 			document.getElementById("registroDni").classList.add("borde_correcto_activo")
 			document.getElementById("registroDni").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error4").classList.add("error_activo")
 			document.getElementById("registroDni").classList.remove("borde_correcto_activo")
 			document.getElementById("registroDni").classList.add("borde_incorrecto_activo")
+			errores.push("4")
 		}
 	break;
 	case "registroTel":
@@ -75,10 +95,14 @@ switch (e.target.name){
 			document.getElementById("p_error5").classList.remove("error_activo")
 			document.getElementById("registroTel").classList.add("borde_correcto_activo")
 			document.getElementById("registroTel").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error5").classList.add("error_activo")
 			document.getElementById("registroTel").classList.remove("borde_correcto_activo")
 			document.getElementById("registroTel").classList.add("borde_incorrecto_activo")
+			errores.push("5")
 		}
 	break;
 	case "registroDir":
@@ -88,10 +112,14 @@ switch (e.target.name){
 			document.getElementById("p_error6").classList.remove("error_activo")
 			document.getElementById("registroDir").classList.add("borde_correcto_activo")
 			document.getElementById("registroDir").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error6").classList.add("error_activo")
 			document.getElementById("registroDir").classList.remove("borde_correcto_activo")
 			document.getElementById("registroDir").classList.add("borde_incorrecto_activo")
+			errores.push("6")
 		}
 	break;
 	case "registroDepto":
@@ -101,10 +129,14 @@ switch (e.target.name){
 			document.getElementById("p_error7").classList.remove("error_activo")
 			document.getElementById("registroDepto").classList.add("borde_correcto_activo")
 			document.getElementById("registroDepto").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error7").classList.add("error_activo")
 			document.getElementById("registroDepto").classList.remove("borde_correcto_activo")
 			document.getElementById("registroDepto").classList.add("borde_incorrecto_activo")
+			errores.push("7")
 		}
 	break;
 	case "registroPostal":
@@ -114,10 +146,14 @@ switch (e.target.name){
 			document.getElementById("p_error8").classList.remove("error_activo")
 			document.getElementById("registroPostal").classList.add("borde_correcto_activo")
 			document.getElementById("registroPostal").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error8").classList.add("error_activo")
 			document.getElementById("registroPostal").classList.remove("borde_correcto_activo")
 			document.getElementById("registroPostal").classList.add("borde_incorrecto_activo")
+			errores.push("8")
 		}
 	break;
 	case "registroLock":
@@ -127,10 +163,14 @@ switch (e.target.name){
 			document.getElementById("p_error9").classList.remove("error_activo")
 			document.getElementById("registroLock").classList.add("borde_correcto_activo")
 			document.getElementById("registroLock").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error9").classList.add("error_activo")
 			document.getElementById("registroLock").classList.remove("borde_correcto_activo")
 			document.getElementById("registroLock").classList.add("borde_incorrecto_activo")
+			errores.push("9")
 		}
 	break;
 	case "registroLockRepeat":
@@ -140,10 +180,14 @@ switch (e.target.name){
 			document.getElementById("p_error10").classList.remove("error_activo")
 			document.getElementById("registroLockRepeat").classList.add("borde_correcto_activo")
 			document.getElementById("registroLockRepeat").classList.remove("borde_incorrecto_activo")
+			while (errores.length >= 1) {
+				errores.pop()	
+			}
 		} else {
 			document.getElementById("p_error10").classList.add("error_activo")
 			document.getElementById("registroLockRepeat").classList.remove("borde_correcto_activo")
 			document.getElementById("registroLockRepeat").classList.add("borde_incorrecto_activo")
+			errores.push("10")
 		}
 	break;
   };
@@ -157,4 +201,17 @@ inputs.forEach((input) => {
 
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
+
+
+  if (errores.length >= 1) {
+
+	alert("Revise el formulario, contiene errores.");
+	  
+  }else {
+	  formulario.submit();
+  }
+
+	
+
 });
+
